0564f420c86cf1b24bfaff9c4a2624da43860b1c
BilalTurabi-786 <muhammadbilalturabi@gmail.com>
2022-10-06 17:55:20 +0500

    init repo

---
+++ producer/app/Console/Commands/PingCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\PingJob;
+use Illuminate\Console\Command;
+class PingCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ping:job';
+    // protected $signature = 'ping:otpcreated';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+            PingJob::dispatch();
+        
+    }
+}
